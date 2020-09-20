9f96456ba0319bbd4dce0c64fea9be7ac87eea91
kristina-gerko <k.gerko007@gmail.com>
2020-09-20 20:53:47 +0300

    feat: add routes, use handlebars as engine

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const router = require('./routes');
+const exhbs = require('express-handlebars');
 
 const port = process.env.PORT || 3000;
 const app = express();
+
+app.use(router);
+
+app.engine('handlebars', exhbs());
+app.set('view engine', 'handlebars');
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
